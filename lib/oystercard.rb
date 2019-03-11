177ba694b676e26ec6e282f3fd44fd153fa622c8
alexanderchen6 <alexbaggio315@hotmail.com>
2019-03-11 15:16:57 +0000

    deduct money

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,18 @@
 class Oystercard
 
     attr_reader :balance
-
+    BALANCE_LIMIT = 90 
     def initialize
         @balance = 0
     end
 
     def top_up(money)
-        @balance += money   
+       raise "cannot top up balance over $#{BALANCE_LIMIT}" if @balance + money > BALANCE_LIMIT
+       @balance += money   
+    end
+
+    def deduct(money)
+        @balance -= money
     end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,9 +4,9 @@ describe Oystercard do
 
     describe ':balance' do
     
-    it 'has a balance of 0 when initialized' do
-        expect(subject.balance).to eq 0
-    end
+        it 'has a balance of 0 when initialized' do
+            expect(subject.balance).to eq 0
+        end
 
     end
 
@@ -16,7 +16,21 @@ describe Oystercard do
             subject.top_up(10)
             expect(subject.balance).to eq 10
         end
+
+        it 'raises an error when over balance limit' do 
+            expect{subject.top_up(100)}.to raise_error "cannot top up balance over $#{Oystercard::BALANCE_LIMIT}"
+        end 
     end
+
+    describe '#deduct' do 
+
+        it 'deducts money from card' do 
+            subject.top_up(20)
+            subject.deduct(10)
+            expect(subject.balance).to eq 10
+        end 
+    end 
+
 
 
 end 
